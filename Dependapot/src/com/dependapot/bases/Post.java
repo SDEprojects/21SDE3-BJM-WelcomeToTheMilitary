f1ed62a2c7dfa1fa9dfe231735d08515507916cb
damianmercado32@gmail.com <61065466+rocky-recon@users.noreply.github.com>
2021-06-30 16:33:09 -0700

    Added bases. Working on Post for multiple bases and Fort Sill map, #AB83

---
+++ Dependapot/src/com/dependapot/bases/Post.java
@@ -0,0 +1,25 @@
+package com.dependapot.bases;
+
+public class Post {
+    private String name;
+    private Map<Direction, Post> adjacentRooms = ...;
+
+    public Room()
+    //    Print current post information:
+    public static void main(String[] args) {
+        current_Building();
+
+    }
+    //    Print post info
+    static void current_Building(){
+
+    }
+//    current post name
+
+    //   get name of post, get enemies, get current building,
+//   get dependa, use text parser
+
+
+}
+
+

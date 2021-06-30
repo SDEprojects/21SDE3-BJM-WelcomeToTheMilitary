61bd9bff554e731d487f84849c63be6d6a1b047b
Jun Park <jhpp114node@gmail.com>
2021-06-30 15:01:02 -0700

    Create textparser and parsereponse, AB#24

--- Dependapot/src/com/dependapot/minigame/DDRKeyboard.java
+++ Dependapot/src/com/dependapot/minigame/DDRKeyboard.java
@@ -1,6 +1,16 @@
 package com.dependapot.minigame;
 
 public class DDRKeyboard implements Minigame {
+    private boolean isWin = false;
+
+    public DDRKeyboard() {
+        //
+    }
+
+    private void instruction() {
+        System.out.println("=".repeat(20));
+        System.out.println("Challenge");
+    }
 
     @Override
     public boolean play() {

797a05d6452b3090c3e9bdef076e8705a8ed2b74
briangonzales10 <brian.gonzales10@gmail.com>
2021-07-20 15:42:55 -0500

    AB398, fixed unit tests

--- test/com/welcomeToTheMilitary/textparser/TextParserTest.java
+++ test/com/welcomeToTheMilitary/textparser/TextParserTest.java
@@ -13,49 +13,31 @@ public class TextParserTest {
     private final String FORTSILL = "Fort Sill";
     private final String FORTBLISS = "Fort Bliss";
 
-    @Test
-    public void receiveActionSuccessFortSill() throws IOException, ParseException {
-        // test for success path for fort sill
-        String successResponse = "go dfac";
-        response = textParser.receiveAction(successResponse,FORTSILL);
-        String actualVerb = response.getVerb();
-        String actualNoun = response.getNoun();
-        assertEquals("go", actualVerb);
-        assertEquals("dfac", actualNoun);
-        System.out.println("receiveActionSuccessForSill Pass");
-    }
 
     @Test
-    public void receiveActionSuccessForInteractNPCFortSill() throws IOException, ParseException {
-        String successResponse = "talk to e-1";
+    public void receiveActionSuccessForInteractNPC() throws IOException, ParseException {
+        String successResponse = "talk to brad";
         response = textParser.receiveAction(successResponse, FORTSILL);
         String actualVerb = response.getVerb();
         String actualNoun = response.getNoun();
         assertEquals("talk", actualVerb);
-        assertEquals("e-1", actualNoun);
+        assertEquals("brad", actualNoun);
     }
 
     @Test
-    public void receiveActionSuccessForBliss() throws IOException, ParseException {
-        String successResponse = "move starbucks";
+    public void receiveActionSuccess() throws IOException, ParseException {
+        String successResponse = "move to Starbucks";
         response = textParser.receiveAction(successResponse, FORTBLISS);
         String actualVerb = response.getVerb();
         String actualNoun = response.getNoun();
+        String expectedVerb = "go";
+        String expectedNoun = "starbucks";
         // System.out.println(actualVerb);
-        assertEquals("move", actualVerb);
-        assertEquals("starbucks", actualNoun);
+        assertEquals(expectedVerb, actualVerb);
+        assertEquals(expectedNoun, actualNoun);
         System.out.println("receiveActionSuccessForBliss Pass");
     }
 
-    @Test
-    public void receiveActionSuccessFortBliss() throws IOException, ParseException {
-        String successResponse = "talk e-7";
-        response = textParser.receiveAction(successResponse, FORTBLISS);
-        String actualVerb = response.getVerb();
-        String actualNoun = response.getNoun();
-        assertEquals("talk", actualVerb);
-        assertEquals("e-7", actualNoun);
-    }
 
     @Test
     public void receiveActionEmptyInputFortSill() throws IOException, ParseException {
@@ -80,7 +62,7 @@ public class TextParserTest {
     }
 
     @Test
-    public void receiveActionNullInputFortSill() throws IOException, ParseException {
+    public void receiveActionNullInput() throws IOException, ParseException {
         // if the input is null somehow, it should return empty string
         String nullResponse = null;
         response = textParser.receiveAction(nullResponse, FORTSILL);
@@ -90,20 +72,9 @@ public class TextParserTest {
         assertEquals("", actualNoun);
     }
 
-    @Test
-    public void receiveActionNullInputFortBliss() throws IOException, ParseException {
-        // testcase for null
-        // it should return empty string for both verb and noun
-        String nullResponse = null;
-        response = textParser.receiveAction(nullResponse, FORTBLISS);
-        String actualVerb = response.getVerb();
-        String actualNoun = response.getNoun();
-        assertEquals("", actualVerb);
-        assertEquals("", actualNoun);
-    }
 
     @Test
-    public void receiveActionInvalidActionFortSill() throws IOException, ParseException  {
+    public void receiveActionInvalidAction() throws IOException, ParseException  {
         // invalid verb
         // invalid verb should return empty string
         String invalidActionVerb = " ge eez";
@@ -114,40 +85,53 @@ public class TextParserTest {
         assertEquals("", actualNoun);
     }
 
-    @Test
-    public void receiveActionInvalidActionFortBliss() throws IOException, ParseException {
-        // invalid verb
-        // should return both verb and noun with empty string
-        String invalidActionVerb = "asdfasdf asdf";
-        response = textParser.receiveAction(invalidActionVerb, FORTBLISS);
-        String actualVerb = response.getVerb();
-        String actualNoun = response.getNoun();
-        assertEquals("", actualVerb);
-        assertEquals("", actualNoun);
-    }
 
     @Test
-    public void receiveActionInvalidNounFortSill() throws IOException, ParseException {
+    public void receiveActionInvalidNoun() throws IOException, ParseException {
         // invalid noun
         // should return both verb and noun with empty string
         String invalidActionNoun = "move asdf";
         response = textParser.receiveAction(invalidActionNoun, FORTSILL);
         String actualVerb = response.getVerb();
         String actualNoun = response.getNoun();
-        assertEquals("move", actualVerb);
-        assertEquals("", actualNoun);
+
+        String expectedVerb = "";
+        String expectedNoun = "";
+        // System.out.println(actualVerb);
+        assertEquals(expectedVerb, actualVerb);
+        assertEquals(expectedNoun, actualNoun);
     }
 
     @Test
-    public void receiveActionInvalidNounFortBliss() throws IOException, ParseException {
-        // invalid noun
-        // should return both verb and noun with empty string
-        String invalidActionNoun = "drive zxbzx";
-        response = textParser.receiveAction(invalidActionNoun, FORTBLISS);
+    public void receiveActionUse() throws IOException, ParseException {
+        // invalid nouns now don't exist with 'use' command.
+        String invalidActionNoun = "use milk";
+        response = textParser.receiveAction(invalidActionNoun, FORTSILL);
         String actualVerb = response.getVerb();
         String actualNoun = response.getNoun();
-        assertEquals("drive", actualVerb);
-        assertEquals("", actualNoun);
+
+        String expectedVerb = "use";
+        String expectedNoun = "milk";
+        // System.out.println(actualVerb);
+        assertEquals(expectedVerb, actualVerb);
+        assertEquals(expectedNoun, actualNoun);
     }
+
+    @Test
+    public void receiveActionUseEmptyFail() throws IOException, ParseException {
+        // invalid noun
+        // should return both verb and noun with empty string
+        String invalidActionNoun = "use";
+        response = textParser.receiveAction(invalidActionNoun, FORTSILL);
+        String actualVerb = response.getVerb();
+        String actualNoun = response.getNoun();
+
+        String expectedVerb = "";
+        String expectedNoun = "";
+        // System.out.println(actualVerb);
+        assertEquals(expectedVerb, actualVerb);
+        assertEquals(expectedNoun, actualNoun);
+    }
+
 
 }

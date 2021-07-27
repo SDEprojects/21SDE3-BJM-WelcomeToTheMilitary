ea225a0b772c055f343d732bf8bfeb5e25f9dbed
briangonzales10 <brian.gonzales10@gmail.com>
2021-07-27 10:49:57 -0500

    AB#517 promotion now displays correctly

--- src/com/welcomeToTheMilitary/gamecontroller/PromoteHelper.java
+++ src/com/welcomeToTheMilitary/gamecontroller/PromoteHelper.java
@@ -3,6 +3,7 @@ package com.welcomeToTheMilitary.gamecontroller;
 import com.welcomeToTheMilitary.character.Enlisted;
 
 import com.welcomeToTheMilitary.character.ServiceMember;
+import com.welcomeToTheMilitary.gui.MainDisplay;
 
 public class PromoteHelper {
 
@@ -11,8 +12,10 @@ public class PromoteHelper {
         return player.getRank().getValue() < soldier.getRank().getValue();
     }
 
-    public static void promote(ServiceMember player, Enlisted soldier) {
+    public static void promote(ServiceMember player, Enlisted soldier) throws InterruptedException {
         player.setRank(soldier.getRank());
         System.out.println("Congratulations! You've been promoted to + " + player.getRank().getAbbreviation());
+        MainDisplay.setMainTextArea("Congratulations! You've been promoted to + " + player.getRank().getAbbreviation());
+        Thread.sleep(200);
     }
 }
